1db150f5eae5ff9ced7ebcc2dba899c5702a88b4
fanney13 <fanney13@skel.ru.is>
2017-10-03 13:15:51 +0000

    prufa

--- src/httpd.c
+++ src/httpd.c
@@ -1,4 +1,24 @@
-/* your code goes here. */
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <ctype.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <glib.h>
+#include <stdbool.h>
+#include <time.h>
+
+typedef enum HttpMethod {GET, HEAD, POST, UNKNOWN} HttpMethod;
+
+const char * const http_methods[] = {
+	"GET",
+	"HEAD",
+	"POST",
+	"UNKNOWN",
+};
 
 int main(int argc, char *argv[])
 {
